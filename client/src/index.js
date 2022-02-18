e56a8ddfc5608aa12fb4fdc586820d4e5a89df20
anton-komarnytskyi <84022832+anton-komarnytskyi-moc@users.noreply.github.com>
2022-02-18 09:48:00 +0200

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -6,7 +6,8 @@ import { ApolloClient, ApolloProvider, InMemoryCache} from '@apollo/client';
 
 const client = new ApolloClient({
   uri: `http://localhost:5000/graphql`,
-  cache: new InMemoryCache()
+  cache: new InMemoryCache(),
+  connectToDevTools: true
 });
 
 
