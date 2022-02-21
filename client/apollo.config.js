eb9b55593277f63c6f8b34c88bd89ae8ac3da75a
Anton Komarnytskyi <anton.komarnytskyi@masterofcode.com>
2022-02-21 15:08:26 +0200

    improve

---
+++ client/apollo.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  client: {
+    service: {
+      name: 'schema',
+      localSchemaFile: './path/to/schema.json',
+    }
+  }
+}
--- client/src/query/user.js
+++ client/src/query/user.js
@@ -1,7 +1,7 @@
 import {gql} from '@apollo/client';
 
 export const GET_ALL_USERS = gql`
-  query{
+  query getAllUsers{
     getAllUsers {
       id, username, age
     }
